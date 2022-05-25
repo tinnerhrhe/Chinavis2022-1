1258a84eaf2d32296f8f6e62bfc59a6c723216ad
LogCreative <lizilong123456@live.cn>
2022-05-25 22:02:50 +0800

    Revert "try to merge"
    This reverts commit 42ec1e041d070862b8a70d7bf2f1b6c76c59f0ca.

--- backend/server.py
+++ backend/server.py
@@ -12,17 +12,17 @@ def root():
 
 search = None
 corenodes = None
-curid = None
 
-@app.route('/mining/<gid>')
-def mining(gid):
-    global search, curid
-    gid = int(gid)
-    curid = gid
-    source = evidence[gid][0]
-    limitation = evidence[gid][1]
+@app.route('/mining/<source>')
+def mining(source):
+    global search
+    limitation = 'small'
+    for e in evidence:
+        if e[0] == source:
+            limitation = e[1]
+            break
     search = searchUCS(Graph(node, link), net_limit[limitation])
-    search.search_run(source)
+    search.run(source)
     subgraph = copy.deepcopy(search.subgraph)
     outputgraph = {
         "nodes": toRecords(subgraph.nodes),
@@ -38,14 +38,6 @@ def corenodes():
         return json.dumps(corenodes)
     return json.dumps({})
 
-@app.route('/stats/<gid>')
-def stats(gid):
-    global search, curid
-    gid = int(gid)
-    if curid == gid and search is not None:
-        return json.dumps(search.statdata)
-    return json.dumps({})
-
 @app.route('/route/<source>')
 def route(source):
     if search is not None and corenodes is not None:
@@ -57,7 +49,6 @@ def route(source):
         paths['visitedPaths'] = pathtracing.visitedEdges
         return json.dumps(paths)
     return json.dumps({'targetPaths':{},'visitedPaths':[]})
-
 
 if __name__ == '__main__':
     app.run()
